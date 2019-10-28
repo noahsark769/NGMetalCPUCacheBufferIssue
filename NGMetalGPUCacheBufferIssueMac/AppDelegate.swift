8666d6c237d62826f6976597b938a027da5fdd01
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-10-27 17:04:27 -0700

    Add mac code demonstrating the issue

---
+++ NGMetalGPUCacheBufferIssueMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NGMetalGPUCacheBufferIssueMac
+//
+//  Created by Noah Gilmore on 10/27/19.
+//  Copyright © 2019 Noah Gilmore. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
